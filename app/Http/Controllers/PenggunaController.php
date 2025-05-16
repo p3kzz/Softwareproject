51a36c45d4ec9edec683db8a3bfcfa080656ad4e
ubay2106 <baykadeo@gmail.com>
2025-05-17 00:24:21 +0700

    template

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -6,7 +6,74 @@ use Illuminate\Http\Request;
 
 class PenggunaController extends Controller
 {
-    public function index(){
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
         return view('dashboard');
+    }
+
+    public function tampil()
+    {
+        return view('pengguna.index');
+    }
+
+    public function menu()
+    {
+        return view('pengguna.menu');
+    }
+
+public function data()
+    {
+        return view('pengguna.data');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
