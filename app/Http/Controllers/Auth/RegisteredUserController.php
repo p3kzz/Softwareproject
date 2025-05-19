28b918dcc78a92c69c193b4b749db7513aa02168
ubay2106 <baykadeo@gmail.com>
2025-05-19 12:14:58 +0700

    tampilan_login_register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,6 +46,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('pengguna.index', absolute: false));
+        return redirect(route('index', absolute: false));
     }
 }
