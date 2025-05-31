be50f32e6ec3924b9cfd4f87d47abaad01e24e27
ubay2106 <baykadeo@gmail.com>
2025-05-31 08:15:32 +0700

    checkout

--- resources/views/checkout/guest-form.blade.php
+++ resources/views/checkout/guest-form.blade.php
@@ -1,9 +1,41 @@
-<form action="{{ route('checkout.store') }}" method="POST">
-    @csrf
-    <input type="text" name="nama" placeholder="Nama" required>
-    <input type="text" name="no_hp" placeholder="No HP" required>
-    <input type="text" value="{{ session('nomor_meja') }}" disabled>
-    <input type="hidden" name="meja_id" value="{{ session('meja_id') }}" required>
-
-    <button type="submit">Checkout</button>
-</form>
+@extends('layouts.pengguna')
+@section('content')
+    <!-- book section -->
+    <section class="book_section layout_padding">
+        <div class="container">
+            <div class="headings_container text-center">
+                <h2 class="text-center">
+                    Biodata Pemesanan
+                </h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="form_container mx-auto">
+                        <form action="{{ route('checkout.store') }}" method="POST">
+                            @csrf
+                            <div>
+                                <label for="">Nama</label>
+                                <input type="text" class="form-control" name="nama" placeholder="Nama" required />
+                            </div>
+                            <div>
+                                <label for="">No Handphone</label>
+                                <input type="text" class="form-control" name="no_hp" placeholder="No HP" required />
+                            </div>
+                            <div>
+                                <label for="">Meja</label>
+                                <input type="text" class="form-control" value="{{ session('nomor_meja') }}" disabled />
+                            </div>
+                            <div>
+                                <input type="hidden" name="meja_id" value="{{ session('meja_id') }}" required>
+                            </div>
+                            <div class="btn_box">
+                                   <button type="submit">Checkout</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end book section -->
+@endsection
